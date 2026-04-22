f9d2f6878fa7a742dbbb4f36df5d6acc5ae80c6d
mfdunlap <maya.dunlap@slu.edu>
2026-04-22 16:28:01 -0500

    Update conditional-redirects.js
    Test four different redirect options

--- test/conditional-redirects.js
+++ test/conditional-redirects.js
@@ -1,5 +1,21 @@
 var nvtag_callbacks = nvtag_callbacks || {};
 nvtag_callbacks.preSegue = nvtag_callbacks.preSegue || [];
 nvtag_callbacks.preSegue.push(function submitSegue(args) {
-  console.log(args.postVals);
+    console.log(args.postVals);
+    var interestVol = args.postVals.Interest_5448342;
+    var activeTest = args.postVals.Interest_5474686;
+    console.log("Volunteer:", interestVol);
+    console.log("Active Test:", activeTest);
+
+    if (activeTest && !interestVol) {
+        window.location = "https://respectmovoters.org/checkin";
+    } else if (interestVol && !activeTest) {
+        window.location = "https://respectmovoters.org/reminders";
+    } else if (activeTest && interestVol) {
+        window.location = "https://respectmovoters.org/dashboard";
+    } else {
+        window.location = "https://respectmovoters.org";
+    }
+
+    return args
 });
