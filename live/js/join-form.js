342355ba0470a292ebf1ddb3c3440a73e9daa060
mfdunlap <maya.dunlap@slu.edu>
2026-04-29 17:35:23 -0500

    simplify join form js and css
    focus just on disabling checkboxes

--- live/js/join-form.js
+++ live/js/join-form.js
@@ -12,50 +12,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     /* =========================
-      Create disabled messgaes
-    ========================= */
-    const volLabel = volCheckbox.closest("label");
-    const notVolLabel = notVolCheckbox.closest("label");
-
-    const volMessage = document.createElement("span");
-    volMessage.className = "checkbox-disabled-message";
-    volMessage.textContent =
-        'Unavailable because "I\'m not ready to volunteer right now" is selected.';
-
-    const notVolMessage = document.createElement("span");
-    notVolMessage.className = "checkbox-disabled-message";
-    notVolMessage.textContent =
-        'Unavailable because "I want to volunteer" is selected.';
-
-
-    /* =========================
-      Remove disabled messgaes
-    ========================= */
-    function removeMessages() {
-        volMessage.remove();
-        notVolMessage.remove();
-
-        volLabel.classList.remove("checkbox-disabled");
-        notVolLabel.classList.remove("checkbox-disabled");
-    }
-
-
-    /* =========================
       Uncheck and disable
       opposite checkbox if
       checked
     ========================= */
     function updateCheckboxLogic() {
-        
-        removeMessages();
 
         // I want to volunteer - Checked
         if (volCheckbox.checked) {
             notVolCheckbox.checked = false;
             notVolCheckbox.disabled = true;
-
-            notVolLabel.classList.add("checkbox-disabled");
-            notVolLabel.appendChild(notVolMessage);
         } else {
             notVolCheckbox.disabled = false;
         }
@@ -64,9 +30,6 @@ document.addEventListener("DOMContentLoaded", function () {
         if (notVolCheckbox.checked) {
             volCheckbox.checked = false;
             volCheckbox.disabled = true;
-
-            volLabel.classList.add("checkbox-disabled");
-            volLabel.appendChild(volMessage);
         } else {
             volCheckbox.disabled = false;
         }
