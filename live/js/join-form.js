1e0528f585d47c9ad97fa3a21ec69513130e716a
mfdunlap <maya.dunlap@slu.edu>
2026-04-29 18:52:47 -0500

    Update join-form.js
    Wait until VAN finishes rendering form fields before looking for elements

--- live/js/join-form.js
+++ live/js/join-form.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     /* =========================
       Find volunteer & not a vol
       checkboxes
