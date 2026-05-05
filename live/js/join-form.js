9bd84f4ef4bec00766cc3cc6f5a8af72e6f10429
mfdunlap <maya.dunlap@slu.edu>
2026-05-05 12:42:00 -0500

    Update new join form
    Fix disabled checkbox css
    Add message for disabled checkbox

--- live/js/join-form.js
+++ live/js/join-form.js
@@ -8,10 +8,9 @@
     const FIELDSET_SELECTOR = '#NVSignupForm2574972-AdditionalInformation';
 
     function initCheckboxLogic(fieldset) {
-        /* =========================
-          Find volunteer & not a
-          vol checkboxes
-        ========================= */
+        /* =================================================
+          Find volunteer & not a volunteer checkboxes
+        ================================================= */
         const checkboxes = fieldset.querySelectorAll('input[type="checkbox"]');
 
         const volCheckbox = Array.from(checkboxes).find(cb =>
@@ -27,16 +26,46 @@
             return;
         }
 
-        /* =========================
-          Uncheck and disable
-          opposite checkbox if
-          checked
-        ========================= */
+
+        /* =================================================
+          Find label attached to each checkbox
+        ================================================= */
+        const volLabel = volCheckbox.closest("label");
+        const volContainer = volLabel.querySelector(".at-checkbox-title-container");
+
+        const notVolLabel = notVolCheckbox.closest("label");
+        const notVolContainer = notVolLabel.querySelector(".at-checkbox-title-container");
+
+
+        /* =================================================
+          Create helper text for disabled checkboxes
+        ================================================= */
+        const volHelper = document.createElement("div");
+        volHelper.className = "checkbox-helper-text";
+        volHelper.textContent = ' Unavailable when "I\'m not ready to volunteer right now" is selected.'
+
+        const notVolHelper = document.createElement("div");
+        notVolHelper.className = "checkbox-helper-text";
+        notVolHelper.textContent = 'Unavailable when "I want to volunteer" is selected.'
+
+
+
+        /* =================================================
+          Uncheck and disable opposite checkbox if checked
+        ================================================= */
         function updateCheckboxLogic(){
+            // Remove all helper text
+            fieldset.querySelectorAll(".checkbox-helper-text").forEach(el => el.remove());
+            
             // I want to volunteer - Checked
             if (volCheckbox.checked) {
                 notVolCheckbox.checked = false;
                 notVolCheckbox.disabled = true;
+                
+                if (notVolContainer) {
+                    notVolContainer.insertAdjacentElement("afterend", notVolHelper);
+                }
+
             } else {
                 notVolCheckbox.disabled = false;
             }
@@ -45,6 +74,11 @@
             if (notVolCheckbox.checked) {
                 volCheckbox.checked = false;
                 volCheckbox.disabled = true;
+                
+                if (volContainer) {
+                    volContainer.insertAdjacentElement("afterend", volHelper);
+                }
+
             } else {
                 volCheckbox.disabled = false;
             }
@@ -57,11 +91,9 @@
 
     }
 
-    /* =========================
-      Watch DOM for VAN
-      injecting the form &
-      initialize logic once
-    ========================= */
+    /* =============================================================
+      Watch DOM for VAN injecting the form & initialize logic once
+    ============================================================= */
     const observer = new MutationObserver((mutations, obs) => {
         const fieldset = document.querySelector(FIELDSET_SELECTOR);
 
--- test/test-js-loader.js
+++ test/test-js-loader.js
@@ -1,7 +1,7 @@
 (function () {
     console.log("NGP VAN loader active...")
     var script = document.createElement("script");
-    script.src = "https://control-kid-willow-defining.trycloudflare.com/live/js/join-form.js?v=" + Date.now();
+    script.src = "https://jacksonville-kerry-emotions-automobiles.trycloudflare.com/live/js/join-form.js?v=" + Date.now();
     document.head.appendChild(script);
     
 })();
