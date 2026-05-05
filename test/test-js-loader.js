eef36bf14cf5bf3f166270031da516f21c109b73
mfdunlap <maya.dunlap@slu.edu>
2026-05-05 13:17:12 -0500

    Create redirects for join form options
    Make different redirects based on join form responses

--- test/test-js-loader.js
+++ test/test-js-loader.js
@@ -1,7 +1,11 @@
 (function () {
     console.log("NGP VAN loader active...")
-    var script = document.createElement("script");
-    script.src = "https://jacksonville-kerry-emotions-automobiles.trycloudflare.com/live/js/join-form.js?v=" + Date.now();
-    document.head.appendChild(script);
+    var script1 = document.createElement("script");
+    script1.src = "https://jacksonville-kerry-emotions-automobiles.trycloudflare.com/live/js/join-form-checkboxes.js?v=" + Date.now();
+    document.head.appendChild(script1);
+
+    var script2 = document.createElement("script");
+    script2.src = "https://jacksonville-kerry-emotions-automobiles.trycloudflare.com/live/js/join-form-redirects.js?v=" + Date.now();
+    document.head.appendChild(script2);
     
 })();
